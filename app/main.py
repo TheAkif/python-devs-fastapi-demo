32bf25211518773d4cfc0858819715e7aea551fb
Akif Hussain <akif.hussaink@gmail.com>
2023-10-02 18:54:31 +0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from starlette.middleware.cors import CORSMiddleware
 
+
 from app.api import api_router
 from app.core.config import settings
 from app.pre_start import init
